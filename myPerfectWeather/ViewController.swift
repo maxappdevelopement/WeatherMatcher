282550cae38a21bb4a72acba024e2b75413c4f02
maxfriman <maxappdevelopement@gmail.com>
2019-02-12 16:41:23 +0100

    Initial Commit

---
+++ myPerfectWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myPerfectWeather
+//
+//  Created by Max Friman on 2019-02-12.
+//  Copyright © 2019 Max Friman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
